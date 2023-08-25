56948c7e6d89f385a47bb23f2ff593e09fe5b822
Rikka-desu <2273379351@qq.com>
2023-08-25 15:04:05 +0800

    [-] 移除无用代码

--- bot.py
+++ bot.py
@@ -1,23 +1,6 @@
 import nonebot
-import os
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
-from nonebot import logger
-from nonebot.log import logger_id, default_filter
-import sys
 
-# 移除 NoneBot 默认的日志处理器
-logger.remove(logger_id)
-# 添加新的日志处理器
-logger.add(
-    sys.stdout,
-    level=0,
-    diagnose=True,
-    format="<g>{time:YYYY-MM-DD HH:mm:ss}</g> [<lvl>{level}</lvl>] <c><u>{name}</u></c> | {message}",
-    filter=default_filter
-)
-
-if not os.path.exists("reimei_databases/"):
-    os.mkdir("reimei_databases/")
 
 nonebot.init()
 
