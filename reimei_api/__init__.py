828409896beb2676babebd9419981c4d5857a404
Rikka-desu <2273379351@qq.com>
2025-10-03 00:32:30 +0800

    更改getIdentityFromQQ为并发式 更改了使用re获取qq号的神比逻辑

--- reimei_api/__init__.py
+++ reimei_api/__init__.py
@@ -6,3 +6,7 @@
 版权所有 (c) 2023 星夕Starset
 依据MIT许可证效力，衍生项目需保证本版权声明原封不动，可以增添“原”前缀以免与您的衍生项目混淆
 """
+from .utils import *
+from .rule import *
+from .help import *
+from .permission import *
--- reimei_api/utils.py
+++ reimei_api/utils.py
@@ -1,13 +1,17 @@
-from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot.adapters.onebot.v11 import Message, Bot
+from nonebot.adapters.onebot.v11 import GroupMessageEvent
 
 import hashlib
+import asyncio
 import zlib
-import httpx
-import base64
 import re
 
+from aiofile import async_open
+import httpx
 
-async def getIdentityFromQQ(qq_list: list[int | str], bot: Bot, group_id: int | str | None = None, *, card: bool = True,
+
+async def getIdentityFromQQ(qq_list: list[int | str], bot: Bot, group_id: int | str | None = None, *,
+                            card: bool = True,
                             do_cache: bool = False) -> list[str]:
     """
     使用多个qq号构建形如多个{昵称}({qq})的文本
@@ -22,16 +26,17 @@ async def getIdentityFromQQ(qq_list: list[int | str], bot: Bot, group_id: int | 
     返回:
       list[str]: 形如"{昵称}({qq})"的字符串的列表
     """
-    res = []
-    if group_id is None or not card:  # 当群号为空且或不使用群昵称时
-        for qq in qq_list:
-            res.append(f'{(await bot.get_stranger_info(user_id=qq, no_cache=not do_cache))["nickname"]}({qq})')
+    if group_id is None or not card:  # 当群号为空或不使用群昵称时
+        tasks = [bot.get_stranger_info(user_id=qq, no_cache=not do_cache) for qq in qq_list]
+        results = await asyncio.gather(*tasks)
+        return [f'{result["nickname"]}({qq})' for qq, result in zip(qq_list, results)]
+
     elif group_id is not None and card:  # 当群号不为空且使用群昵称时
-        for qq in qq_list:
-            res.append(
-                f'{(await bot.get_group_member_info(user_id=qq, group_id=group_id, no_cache=not do_cache))["card"]}({qq})'
-            )
-    return res
+        tasks = [bot.get_group_member_info(user_id=qq, group_id=group_id, no_cache=not do_cache) for qq in qq_list]
+        results = await asyncio.gather(*tasks)
+        return [f'{result["card"]}({qq})' for qq, result in zip(qq_list, results)]
+
+    return []
 
 
 async def getNicknameFromQQ(qq: int | str, bot: Bot, group_id: int | str | None = None, *, card: bool = True,
@@ -108,19 +113,7 @@ async def key_value2dict(text: str) -> dict | str:
 
 
 
-async def get_at(string: str) -> int:
-    """
-    获取at消息中的QQ号
-    返回-1则为消息中不存在at信息或at信息格式错误
-    :return: int
-    """
-    try:
-        return int(re.findall(r"\[CQ:at,qq=([0-9]+)\]", string)[0])
-    except:
-        return -1
-
-
-async def def CQ2dict(string: str) -> dict:
+async def CQ2dict(string: str) -> dict:
     """
     把单个CQ码转换为onebot的消息段格式的dict
     :return: dict
@@ -137,7 +130,7 @@ async def def CQ2dict(string: str) -> dict:
     return CQ_json
 
 
-async def def msg2list(msg: Message | str) -> list:
+async def msg2list(msg: Message | str) -> list:
     """
     把Message转换为onebot的消息段数组格式的list
     :return: list
@@ -159,61 +152,20 @@ async def def msg2list(msg: Message | str) -> list:
     return msg_list
 
 
-async def def key_value2dict(text: str) -> dict | str:
+async def get_at(msg: Message) -> int:
     """
-    把传入的形如"aaa=bbb ccc=ddd"的字符串转换为形如{'aaa':'bbb','ccc','ddd'}的dict
-
-    参数:
-        text (str): 需要处理的字符串
-
-    返回:
-        dict: 处理完成的dict
-        str: 错误信息
+    获取at消息中的QQ号
+    返回-1则为消息中不存在at信息或at信息格式错误
+    :return: int
     """
-    try:
-        text = text.strip()
-        return_value = dict([i.split("=") for i in text.split(" ")])
-        print(return_value)
-        return return_value
-    except BaseException as error:
-        return str(error)
+    at = msg["at"]
+    if not msg:
+        return at[0].data["qq"]
+    else:
+        return -1
 
 
-async def def calculate_sha256(file_path: str) -> str:
-    """
-    读取文件内容并计算SHA512哈希值。
-
-    参数:
-        file_path (str): 要读取的文件的路径。
-
-    返回:
-        str: 文件的SHA512哈希值。
-    """
-    sha256_hash = hashlib.sha256()
-    with open(file_path, "rb") as f:
-        for byte_block in iter(lambda: f.read(4096), b""):
-            sha256_hash.update(byte_block)
-    return sha256_hash.hexdigest()
-
-
-async def def calculate_crc32(file_path: str) -> str:
-    """
-    读取文件内容并计算CRC32哈希值，并以16进制表示。
-
-    参数:
-        file_path (str): 要读取的文件的路径。
-
-    返回:
-        str: CRC32的十六进制表示
-    """
-    crc32_hash = 0
-    with open(file_path, 'rb') as file:
-        for chunk in iter(lambda: file.read(4096), b""):
-            crc32_hash = zlib.crc32(chunk, crc32_hash)
-    return f"{crc32_hash & 0xffffffff:0x}"
-
-
-async def def in_type(args: Message, type_: str) -> bool:
+async def in_type(args: Message, type_: str) -> bool:
     """
     检测传入Message中是否有at消息
 
@@ -223,27 +175,10 @@ async def def in_type(args: Message, type_: str) -> bool:
     返回:
         bool: 是否存在at消息
     """
-    for i in args:
-        if i.type == type_:
-            return True
-    return False
-
-
-async def def get_at(args: Message) -> int | str:
-    """
-    获取传入Message的第一个at消息的qq
-
-    参数:
-        args (Message): 需要提取的消息段
-
-    返回:
-        str: 提取出来的qq号
-        str： 错误信息
-    """
-    try:
-        return args["at"][0].data["qq"]
-    except BaseException as error:
-        return str(error)
+    if not args[type_]:
+        return True
+    else:
+        return False
 
 
 async def args_qq(args: Message) -> str | None:
@@ -260,7 +195,7 @@ async def args_qq(args: Message) -> str | None:
     if args.extract_plain_text().isdigit():
         return args.extract_plain_text()
     if in_type(args, "at"):
-        return get_at(args)
+        return str(await get_at(args))
     else:
         return None
 
@@ -283,4 +218,41 @@ async def _async_download_content(url: str) -> bytes:
     return data
 
 
+async def calculate_sha256(file_path: str) -> str:
+    """
+    读取文件内容并计算SHA256哈希值。
 
+    参数:
+        file_path (str): 要读取的文件的路径。
+
+    返回:
+        str: 文件的SHA256哈希值。
+    """
+    sha256_hash = hashlib.sha256()
+    async with async_open(file_path, "rb") as f:
+        while True:
+            byte_block = await f.read(4096)
+            if not byte_block:
+                break
+            sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
+
+
+async def calculate_crc32(file_path: str) -> str:
+    """
+    读取文件内容并计算CRC32哈希值，并以16进制表示。
+
+    参数:
+        file_path (str): 要读取的文件的路径。
+
+    返回:
+        str: CRC32的十六进制表示
+    """
+    crc32_hash = 0
+    async with async_open(file_path, 'rb') as file:
+        while True:
+            chunk = await file.read(4096)
+            if not chunk:
+                break
+            crc32_hash = zlib.crc32(chunk, crc32_hash)
+    return f"{crc32_hash & 0xffffffff:08x}"
